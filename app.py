a1414fc9debf78d1b49d10634c9e3e8da1cd8868
subramani2701 <subramanikumar2701@gmail.com>
2024-07-28 01:08:26 +0530

    feat:Form data and request object

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask,redirect, render_template,url_for,request
 
 app = Flask(__name__)
 
-@app.route('/hello')
 
 # @app.route('/login',methods = ['POST', 'GET'])
 # def login():
@@ -18,11 +17,25 @@ app = Flask(__name__)
 def hello_name(user):
    return render_template('hello.html',name=user)
 
-@app.route('/result')
+@app.route('/marks')
 def result():
    dict = {'phy':50,'che':60,'maths':70}
-   return render_template('results.html', result = dict)
+   return render_template('marks.html', result = dict)
 
+
+# @app.route("/")
+# def index():
+#    return render_template("index.html")
+
+@app.route('/')
+def student():
+   return render_template('student.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def new_result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("new_results.html",result = result)
 
 if __name__ == '__main__':
    app.run(debug=True)
